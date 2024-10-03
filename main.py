99f520960c146de951eeccf5ad3195709c928975
Chello <christinbertelsen@gmail.com>
2024-10-03 14:49:59 +0200

    Legger til JOhanne og gtuppr 9

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     html = "<h1>Andreas er mye bedre enn Nicolai</h1><p>Denne nettsiden inneholder fakta</p>"
-    j = "<h2>Men Johanne er best<h2>"
+    html += "<h2>Men Johanne og gruppe 9 er best<h2>"
     
     currentDateAndTime = datetime.now()
 
     html += "The current date and time is  " + str(currentDateAndTime)
     for i in range(1,101):
         html += "<h3>Dette er linje nr: " + str(i) + "<h3>"
-    return html,j
+    
+    return html 
 
 # Define a route for a custom page
 @app.route('/about')
