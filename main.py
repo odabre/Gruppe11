db18720da3971e946437b1cad0b4f1712d556ad8
Chello <christinbertelsen@gmail.com>
2024-10-10 13:22:34 +0200

    startet main

--- main.py
+++ main.py
@@ -8,27 +8,6 @@ app = Flask(__name__, static_folder='static')
 
 
 
-# Define a route for the home page
-@app.route('/')
-def home():
-    html = "<h1>Andreas er mye bedre enn Nicolai</h1><p>Denne nettsiden inneholder fakta</p>"
-    html += "<h2>Men Johanne og gruppe 9 er best!<h2>"
-    html += """<a href="oda"> Oda</a> <a href="chello"> chello </a>"""
-    
-    currentDateAndTime = datetime.now()
-
-    html += "The current date and time is  " + str(currentDateAndTime)
-    for i in range(1,101):
-        html += "<h3>Dette er linje nr: " + str(i) + "<h3>"
-    
-    return html 
-
-# Define a route for a custom page
-@app.route('/about')
-def about():
-    return "NicolaiErBadass"
-    #return render_template("nicolaiermegabadassogkjekk.html")
-
 @app.route('/oda')
 def oda_func():
     return oda.run()
@@ -44,3 +23,32 @@ if __name__ == '__main__':
 
 #  git config --global user.email "you@example.com"
 #  git config --global user.name "Your Name"
+
+
+@app.route('/')
+def run():
+    return """
+    <html>
+        <head>
+            <style>
+                p.bold {
+                    font-weight: bold;
+                }
+                .blåfarge {
+                    color: green}
+                h1 {
+                    font_family
+                }
+
+            </style>
+        </head>
+        <body>
+            <h2>Mausund nettside yeah</h2>
+            <h2>dette blir bra</h2>
+            <h1>Sensor greier</h1>
+            <p>Noen bilder kanskje<p>
+            <p class="blåfarge">Grafer her? Eller på en ny side muligens?</p>
+
+        </body>
+    </html>
+"""
