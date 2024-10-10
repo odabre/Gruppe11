1edc09da57f95c5106d9e2159490712197e5e69a
Chello <christinbertelsen@gmail.com>
2024-10-10 14:46:37 +0200

    ninja greier

--- main.py
+++ main.py
@@ -1,37 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 from time import time
 import chello
 import oda
+import os
 # Create an instance of the Flask class
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 
 @app.route('/')
 def run():
-    return """
-    <html>
-        <head>
-            <title>Mausund</title>
-            <style>
-                body {
-                    background-color: lightblue;
-                }
-            </style>
-        </head>
-        <body>
-        </body>
-    </html>
-"""
+    return render_template("test.html")
 
+
+lol = ["nicolai", "oda", "chello"]
 
 @app.route('/oda')
 def oda_func():
-    return oda.run()
+    return render_template("oda.html", liste = lol)
 
 
 @app.route('/chello')
 def chello_func():
-    return chello.run()
+    return render_template("chello.html")
 
 # Run the application
 if __name__ == '__main__':
